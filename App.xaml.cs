c7f8a9376bd2413240b4ab828daa97d5b8256759
Jason Michel YAWILHIT <67412667+kmkz-69@users.noreply.github.com>
2020-08-08 00:00:39 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Chatr.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Chatr
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                ViewModel.SplashScreen Splash = new ViewModel.SplashScreen();
+                if (Settings.Show_SplashScreen)
+                { Splash.Show(); }
+              
+
+                if (File.Exists(Settings.KeepMe) && File.Exists(Settings.SessionID) && File.Exists(Settings.UserID))
+                {
+
+                    string aa = File.ReadAllText(Settings.UserID);
+                    string bb = File.ReadAllText(Settings.SessionID);
+                    string dd = File.ReadAllText(Settings.KeepMe);
+                    string TextID = aa.Replace("\r\n", "");
+                    string sessionIDText = bb.Replace("\r\n", "");
+                    string keepmeText = dd.Replace("\r\n", "");
+
+                    if (keepmeText == "1" && sessionIDText != "" && TextID != "")
+                    {
+                        //MainForm Mainform = new MainForm();
+                        //Thread.Sleep(1000);
+                        Splash.Close();
+                        MainWindow.Show();
+                    }
+                    else
+                    {
+                        MainWindow SignIn = new MainWindow();
+                        Thread.Sleep(1000);
+                        Splash.Close();
+                        SignIn.ShowDialog();
+                    }
+
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(Settings.FolderDestination + "\\" + Settings.AppName);
+                    string[] lines3 = { "0" }; System.IO.File.WriteAllLines(Settings.KeepMe, lines3);
+                    string[] lines2 = { "" }; System.IO.File.WriteAllLines(Settings.SessionID, lines2);
+                    string[] lines = { "" }; System.IO.File.WriteAllLines(Settings.UserID, lines);
+                    MainWindow SignIn = new MainWindow();
+                    SignIn.ShowDialog();
+                }
+
+
+            }
+            catch { }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Chatr.ViewModel;
+using Chatr.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chatr
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            var signup = new SignUp();
+            signup.Show();
+            this.Close();
+        }
+
+        private void Password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Tab)
+            {
+                if (Password.Password == "")
+                {
+                    Password.Password = "Password";
+                    Password.Foreground = new SolidColorBrush();
+                }
+            }
+
+        }
+
+        private void Username_KeyDown(object sender, KeyEventArgs e)
+        {
+            var uie = e.OriginalSource as UIElement;
+            if(e.Key == Key.Enter)
+            {
+                if(Username.Text == "")
+                {
+                    Username.Text = "Username / Email";
+                    Username.Foreground = new SolidColorBrush();
+                }
+                e.Handled = true;
+                uie.MoveFocus(
+                    new TraversalRequest(FocusNavigationDirection.Previous));
+            }
+        }
+
+        private void Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(Password.Password == "Password")
+            {
+                Password.Password = "";
+                Password.Foreground = new SolidColorBrush();
+            }
+        }
+
+        private void FortgotPassword_Click(object sender, RoutedEventArgs e)
+        {
+            var resetPassword = new ResetPassword();
+            resetPassword.Show();
+            this.Close();   
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+           var contentMain = new ContentMain();
+           contentMain.Show();
+           this.Close();
+        }
+    }
+}
